c83cd20b6c64c5c5c37bc5c3a6da685738d9cb03
daniel watkins <danielmwatkins@gmail.com>
2024-06-28 16:15:15 -0400

    continued updates

---
+++ scripts/05_filter_geotiffs.py
@@ -0,0 +1,1 @@
+"""This function will use the results of 04 and 02, reading the GeoTiff images with floe shapes and making a copy that only includes the floe shapes that were classified as likely floes by the logistic regression function OR were manually assigned as true positives based on tracking."""
